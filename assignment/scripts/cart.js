5f06f1c56f5e473ffb663113d1a9e13de19f505b
Philip Krynicki <philipkrynicki@yahoo.com>
2021-04-25 15:45:36 -0500

    Completed Assignment 3, Week 4

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,32 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem (item) {
+  basket.push(item);
+  return true;
+}
+
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
+function listItems () {
+  for (let i = 0; i < basket.length; i++) {
+    console.log(basket[i]);
+  }
+}
+
+addItem('mangoes');
+addItem('kiwi');
+
+listItems();
+
+
+function empty (){
+  basket = [];
+}
+console.log('item has been removed');
+empty();
